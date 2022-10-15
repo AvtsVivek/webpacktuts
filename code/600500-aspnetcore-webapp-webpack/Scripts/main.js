897574f5ca3415009f4e03d7b7984fe2218b8083
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-10-15 14:48:31 +0530

    Added more info

--- code/600500-aspnetcore-webapp-webpack/Scripts/main.js
+++ code/600500-aspnetcore-webapp-webpack/Scripts/main.js
@@ -2,7 +2,7 @@ var other = require('./other');
 
 global.jQuery = global.$ = require('jquery');
 
-jQuery('body').append('<p>Hello World! If you are seeing this, then jquery is loaded....</p>');
+jQuery('#jqueryTestDiv').append('<p>Hello World! If you are seeing this, then jquery is loaded....</p>');
 
 alert('Here we go');
 
